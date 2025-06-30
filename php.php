7a08a5c1c76318d059c23518ad1d07f670762bc3
Yuan Axl Alcuetas <yuanalcuetas2006@gmail.com>
2025-06-30 22:06:51 +0700

    Files

---
+++ php.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Document</title>
+</head>
+<body class="bg-gray-300">
+    <div class="min-h-screen flex justify-center items-center">
+        <div class="grid grid-cols-1 md:grid-cols-2 w-[90%] md:w-[80%] gap-4">
+            <div class="w-full bg-white rounded p-8 shadow space-y-4 gap-4">
+                <h1 class="text-lg lg:text-2xl text-gray-800 font-bold text-center">FORMS:</h1>
+                <form action="" class="space-y-4 space-x-2 flex flex-col justify-center items-center w-full">
+                    <div class="flex justify-center flex-col w-[90%] md:w-[80%] lg:w-[70%]">
+                        <label for="" class="text-lg lg:text-xl text-gray-800 font-bold my-2">Firstname:</label>
+                        <input type="text" class="px-4 py-2 border border-2 border-gray-800 rounded w-full">
+                    </div>
+                    <div class="flex justify-center flex-col w-[90%] md:w-[80%] lg:w-[70%]">
+                        <label for="" class="text-lg lg:text-xl text-gray-800 font-bold">Gmail:</label>
+                        <input type="text" class="px-4 py-2 border border-2 border-gray-800 rounded w-full ">
+                    </div>
+                    <div class="flex justify-center flex-col w-[90%] md:w-[80%] lg:w-[70%]">
+                        <label for="" class="text-lg lg:text-xl text-gray-800 font-bold">Location:</label>
+                        <input type="text" class="px-4 py-2 border border-2 border-gray-800 rounded w-full">
+                    </div>
+                </form>
+                    <div class="flex justify-center">
+                        <button type="submit" class="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 w-[30%] rounded">Save</button>
+                    </div>
+            </div>
+            <div class="w-full bg-white rounded p-8 shadow">
+                <h1 class="text-lg lg:text-2xl text-gray-800 font-bold text-center my-4">TABLE:</h1>
+                <div class="overflow-auto">
+                    <table class="min-w-full text-sm border border-gray-800">
+                        <thead class="bg-gray-200 text-gray-800 font-bold">
+                            <tr>
+                                <th class="px-4 py-2 border text-start">Firstname</th>
+                                <th class="px-4 py-2 border text-start">Gmail</th>
+                                <th class="px-4 py-2 border text-start">Location</th>
+                                <th class="px-4 py-2 border text-start">Time-in</th>
+                                <th class="px-4 py-2 border text-start">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-gray-200 text-gray-800 font-bold">
+                            <?php
+                                $connection = new mysqli("localhost", "root", "", "php");
+
+                                if ($connection->connect_error) {
+                                    die("Connection failed: " . $connection->connect_error);
+                                }
+
+                                $sql = "SELECT * FROM Info";
+                                $result = $connection->query($sql);
+
+                                if (!$result) {
+                                    die("Invalid Query: " . $connection->error);
+                                }
+
+                                while ($row = $result->fetch_assoc()) {
+                                    echo "
+                                    <tr class='hover:bg-gray-50'>
+                                        <td class='px-4 py-2 border text-start'>{$row['Firstname']}</td>
+                                        <td class='px-4 py-2 border text-start'>{$row['Gmail']}</td>
+                                        <td class='px-4 py-2 border text-start'>{$row['Location']}</td>
+                                        <td class='px-4 py-2 border text-start'>{$row['Time_in']}</td>
+                                        <td class='px-4 py-2 border text-start flex justify-center items-center gap-2'>
+                                            <button type='submit' class='bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded'>Update</button>
+                                            <button type='submit' class='bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded'>Delete</button>
+                                        </td>
+                                    </tr>";
+                                }
+                                ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
